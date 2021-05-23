b4c15c5dae829f5bee013f54c90e7e7e476e4569
angry-pineapple <rish@live.in>
2021-05-23 18:23:42 +0530

    Tower of Hanoi

--- Problems/q19.py
+++ Problems/q19.py
@@ -1,33 +1,33 @@
 # Tower of Hanoi
 
 def toh(n, source, destination, helper):
-    # if 3 disks-
-    if n == 0:
-        return
-    toh(n-1, source, helper, destination)
-    print(f"moving disk {n} to --> {destination}")
-    toh(n-1, helper, destination, source)
+    # if n == 0:
+    #     return
+    # toh(n-1, source, helper, destination)
+    # print(f"moving disk {n} to --> {destination}")
+    # toh(n-1, helper, destination, source)
+    if n>0:
+        toh(n - 1, source, helper, destination)
+        print(f"moving disk {n} to --> {destination}")
+        toh(n - 1, helper, destination, source)
 
-def toh_stack(source, destination, helper):
-    if source == []:
-        return
-    # [1,2,3] [] []
-    # [1,2] [] []
-    # [1] [] []
-    # [1*, 2] [] [1]
-    # [1*, 2*] [2] [1]
-    # [1* 2*] [2,1] []
-    # [1*,2* 3] [2, 1] []
-    # [1* 2 * 3*] [2, 1] [3]
-    # [1* 2 * 3*] [ 3, 2, 1] []
-    toh_stack(source[:-1])
+def toh_stack(n,start, destination, helper):
+    if n > 0:
+        toh_stack(n - 1, start, helper, destination)
+        destination.append(start.pop())
+        toh_stack(n - 1, helper, destination, start)
+
 
 if __name__ == "__main__":
     number_of_disks = 3
-    # A,B,C=[],[],[]
     source, helper, destination = 'source', 'helper', 'destination'
-    # toh(number_of_disks, source, destination, helper)
-    s, d, h = [1, 2, 3], [], []
+    print("##### Steps ######################")
+    toh(number_of_disks, source, destination, helper)
+    s, d, h = [3,2,1], [], []
+    print("###################################")
+    print("\n######### Using stack/array: #####")
+    print("before->",s,d,h)
+    toh_stack(len(s),s, d, h)
+    print("after-> ",s,d,h)
+    print("###################################")
 
-    a,b,c = toh_stack(s, d, h)
-    print(a,b,c)
